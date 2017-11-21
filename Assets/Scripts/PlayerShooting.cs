cfcb40d15e97eff05f0498cdf12f480b7b66cd21
Jaire Vaughn <Jaire.Vaughn@iam.local>
2017-11-20 18:24:24 -0600

    Finished Game

---
+++ Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour {
+
+    public Vector3 bulletOffset = new Vector3(0, 0.5f, 0);
+    public GameObject bulletPrefab;
+    public Transform muzzleRotateAnchor;
+    public Transform rocketMuzzle;
+    public float fireRate;
+    public float nextFire = 0f;
+
+
+    // Use this for initialization
+    private void PlayerShoot()
+    {
+        if (Input.GetAxisRaw("Fire1") > 0) fireBullet();
+    }
+    void fireBullet()
+    {
+        if (Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Vector3 offset = transform.rotation * bulletOffset;
+            GameObject newbullet = Instantiate(bulletPrefab, rocketMuzzle.position, muzzleRotateAnchor.rotation);
+        }
+    }
+
+            // Update is called once per frame
+    void Update ()
+    {
+        PlayerShoot();
+	}
+}
---
+++ Assets/Scripts/RespawnPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RespawnPlayer : MonoBehaviour {
+
+    public static event Action PlayerRespawnedFromCheckpoint;
+    public void Respawn()
+        
+    {
+       
+            if (Checkpoint.currentlyActiveCheckpoint == null)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+            else
+            {
+                transform.position =
+                    Checkpoint.currentlyActiveCheckpoint.transform.position;
+            //if (PlayerRespawnedFromCheckpoint)
+            //{
+
+            //}
+            }
+        }
+    }
